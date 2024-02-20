4feb1e17b125034636fb7bec47b413f5a908e40f
Nguyen Ngoc Bach <bachnn92@gmail.com>
2024-02-20 15:24:58 +0700

    python update

---
+++ Groovy/main.groovy
@@ -0,0 +1,17 @@
+int score = 75
+switch (score){
+    case 90 100:
+        print("sehr Gut")
+        break
+    case 80 89:
+        print("Gut")
+        break
+    case 70 79:
+        print("Befriedigend")
+        break
+    case 60 69:
+        print("Ausreichend")
+        breaks
+    default:
+        print("nicht Bestanden")
+}
